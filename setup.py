d67de4dcc9eb82d8737425dbc7ae96fc22614d25
dinesh578 <94045187+dinesh578@users.noreply.github.com>
2023-01-25 06:30:25 +0530

    test and created a package called src

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup , find_packages
+
+setup(name ="src",
+      version="0.01",
+      description="mlops package",
+      packages=find_packages(),
+      author="din578")
--- src/get_data.py
+++ src/get_data.py
@@ -3,6 +3,7 @@ import yaml
 import pandas as pd
 import argparse
 
+
 def read_params(config_path):
     with open(config_path) as yaml_file:
         config = yaml.safe_load(yaml_file)
